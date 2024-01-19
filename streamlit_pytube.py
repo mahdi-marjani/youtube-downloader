a7c9a66dacb20b4ac067a31a0518958daf8071de
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2024-01-19 14:25:57 +0330

    Using radio instead of selectbox

--- streamlit_pytube.py
+++ streamlit_pytube.py
@@ -3,7 +3,6 @@ from pytube import YouTube
 from io import BytesIO
 import base64
 
-
 st.title("YouTube Downloader")
 
 video_url = st.text_input("Video URL:")
@@ -23,7 +22,9 @@ if st.button("get info"):
         st.warning("Please enter a valid URL")
 
 if st.session_state != {}:
-    video_format = st.selectbox("Video Format:", st.session_state[0])
+    selected_video_format_index = st.radio("Video Format:", range(len(st.session_state[0])))
+    video_format = st.session_state[0][selected_video_format_index]
+
     if st.button("download"):
         with st.spinner(text="downloading"):
             buffer = BytesIO()
@@ -31,9 +32,9 @@ if st.session_state != {}:
 
             # Convert the video data to base64
             video_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-
-        # Display the video using HTML5 video tag
-        st.markdown(f'<video controls width="640" height="360" src="data:video/mp4;base64,{video_base64}" type="video/mp4"></video>', unsafe_allow_html=True)
+        
+        with st.spinner(text="displaying video"):
+            # Display the video using HTML5 video tag
+            st.markdown(f'<video controls width="640" height="360" src="data:video/mp4;base64,{video_base64}" type="video/mp4"></video>', unsafe_allow_html=True)
 
         st.success("Successfully Downloaded")
-
